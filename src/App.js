bb46a787d74dcbaba82c8d1f0d8e6f4071a11720
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-10-23 17:07:33 +0100

    Add form-component and users-component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
-import Navbar from './components/AppNavbar';
+import Navbar from "./components/AppNavbar";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import RegistrationForm from "./components/RegistrationForm";
+import Users from "./components/Users";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className='App'>
-    <Navbar />
-      <h1>Enye Form </h1>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/register' component={RegistrationForm} />
+        <Route exact path='/users' component={Users} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -1,22 +1,35 @@
 import React from "react";
 import Logo from "../img/enye.png";
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar } from "react-bootstrap";
 
 const AppNavbar = () => {
   return (
-    <Navbar bg='light' expand='lg'>
+    <Navbar bg='info' expand='lg' variant='dark'>
       <Navbar.Brand href='/'>
-        <img src={Logo} alt={"Enye logo"} style={{width: '100px'}} />
+        <img src={Logo} alt={"Enye logo"} style={{ width: "100px" }} />
       </Navbar.Brand>
+      <h1
+        style={{
+          color: "#fff",
+          marginLeft: "2rem",
+          textTransform: "uppercase"
+        }}
+      >
+        Registration Portal
+      </h1>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className='mr-auto'>
-          <Nav.Link href='/'>Home</Nav.Link>
-          <Nav.Link href='/register'>Register</Nav.Link>
-          <Nav.Link href='/users'>Users</Nav.Link>
-          
+        <Nav className='ml-auto' style={{ fontSize: "2rem" }}>
+          <Nav.Link href='/' style={{ color: "#fff" }}>
+            Home
+          </Nav.Link>
+          <Nav.Link href='/register' style={{ color: "#fff" }}>
+            Register
+          </Nav.Link>
+          <Nav.Link href='/users' style={{ color: "#fff" }}>
+            Users
+          </Nav.Link>
         </Nav>
-        
       </Navbar.Collapse>
     </Navbar>
   );
